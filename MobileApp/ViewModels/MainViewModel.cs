484ef91e22ae3712ce3ebdb0804f84f38e986d06
koffela <46707662+koffela@users.noreply.github.com>
2024-06-18 14:03:42 -0700

    Added formatting, navigation, functionality button
    Formatted the entire UI with a grid, and added a navigation menu with four tabs that are attached at the bottom of the screen. Additionally, added functionality to the button so that it can modify the text inside the text box.

--- MobileApp/ViewModels/MainViewModel.cs
+++ MobileApp/ViewModels/MainViewModel.cs
@@ -1,6 +1,48 @@
-﻿namespace MobileApp.ViewModels;
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml.MarkupExtensions;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System.ComponentModel;
+using System.Windows.Input;
 
-public class MainViewModel : ViewModelBase
+namespace MobileApp.ViewModels
 {
-    public string Greeting => "Custom App V1.0.1";
+    public class MainViewModel : ViewModelBase, INotifyPropertyChanged //needed this to update UI
+    {
+        private string _myTextProperty;
+        public string MyTextProperty
+        {
+            get => _myTextProperty;
+            set
+            {
+                _myTextProperty = value;
+                OnPropertyChanged(nameof(MyTextProperty)); // Notify UI that property has changed NEEDED as above
+            }
+        }
+
+        public string Greeting => "Custom App V1.0.1";
+
+        public MainViewModel()
+        {
+            // Create a command that will be executed when the button is clicked
+            ButtonClickCommand = ReactiveCommand.Create(ButtonClick);
+        }
+
+        public ICommand ButtonClickCommand { get; }
+
+        private void ButtonClick()
+        {
+            // Code to be executed when the button is clicked
+            // For example, set the text of the text box
+            MyTextProperty = "Button clicked!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) //the actual implementation of 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
